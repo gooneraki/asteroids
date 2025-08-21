9eb52d285f4b11f55bb99671b2c135484d6eaf3d
gooneraki <hayiate4@hotmail.com>
2025-08-21 23:19:45 +0300

    rotate

--- main.py
+++ main.py
@@ -1,34 +1,29 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
 from player import Player
 
-def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
+def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-   
-    dt = 0
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
 
-        player1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
-        player1.draw(screen)
-        
+        player.update(dt)
+
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
-        time_passed = clock.tick(60)
-        dt = time_passed / 1000
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
