6ec373cb0b65115f8cfcf921ee457f63a210ca7e
gooneraki <hayiate4@hotmail.com>
2025-08-22 00:21:33 +0300

    rate limit

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
@@ -35,6 +36,8 @@ class Player(CircleShape):
 
         if keys[pygame.K_SPACE]:
             self.shoot()
+        
+        self.timer -= dt
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
@@ -44,6 +47,9 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
+        if self.timer > 0:
+            return
         shot = Shot(self.position.x, self.position.y)
         shot.velocity =  pygame.Vector2(0,1).rotate(self.rotation ) * PLAYER_SHOOT_SPEED
+        self.timer = PLAYER_SHOOT_COOLDOWN
 
